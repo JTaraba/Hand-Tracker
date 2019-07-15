e2cf77f18b6b4c1e4f849517f17b82323563036d
Josh Taraba <jwtaraba@gmail.com>
2019-07-15 10:44:35 -0400

    writing some javascript adding all the fuctions

---
+++ script.js
@@ -0,0 +1,26 @@
+//This is for differentiating between browsers
+navigator.getUserMedia = navigator.getUserMedia || 
+navigator.webkitGetUserMedia ||
+navigator.mozGetUSerMedia ||
+navigator.msGetUserMedia;
+
+//initializing html
+const video = document.querySelector('#video');
+const audio = document.querySelector('#audio');
+const canvas = document.querySelector('#canvas');
+const canvas = canvas.getContext('2d');
+let model;
+
+handTrack.startVideo(video)
+    .then(status => {
+        if (status) {
+            navigator.getUserMedia({video:{}}, stream=> {
+
+            },
+            err => console.log(err));
+        }
+    })
+
+handTrack.load().then(lmodel => {
+    model = lmodel;
+});
