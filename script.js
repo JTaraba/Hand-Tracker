7dfa9c19a9a699cb14abb8a0754bb8a5e23f216c
Josh Taraba <jwtaraba@gmail.com>
2019-07-15 11:00:01 -0400

    fixed webcam not showing up, working in chrome now

--- script.js
+++ script.js
@@ -8,19 +8,20 @@ navigator.msGetUserMedia;
 const video = document.querySelector('#video');
 const audio = document.querySelector('#audio');
 const canvas = document.querySelector('#canvas');
-const canvas = canvas.getContext('2d');
+const context = canvas.getContext('2d');
 let model;
 
 //Opens up the webcam
-handTrack.startVideo(video)
-    .then(status => {
-        if (status) {
-            navigator.getUserMedia({video:{}}, stream=> {
+handTrack.startVideo(video).then(status => {
+    if (status) {
+        navigator.getUserMedia(
+            {video:{}}, 
+            stream => {
                 video.scrObject = stream;
             },
-            err => console.log(err));
-        }
-    })
+        err => console.log(err));
+    }
+})
 
 handTrack.load().then(lmodel => {
     model = lmodel;
